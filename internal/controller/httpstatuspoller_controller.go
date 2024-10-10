ed516ccf81a1d3af4dd453b155a4b6e119c60ea6
Francesco Giudici <francesco.giudici@suse.com>
2024-10-10 18:41:25 +0200

    polling: be more verbose on success
    moreover, don't throw an error if the url is not responding but just
    report it is unreachable
    
    Signed-off-by: Francesco Giudici <francesco.giudici@suse.com>

--- internal/controller/httpstatuspoller_controller.go
+++ internal/controller/httpstatuspoller_controller.go
@@ -19,6 +19,7 @@ package controller
 import (
 	"context"
 	"net/http"
+	"net/url"
 	"time"
 
 	"k8s.io/apimachinery/pkg/runtime"
@@ -80,17 +81,22 @@ func pollURLs(ctx context.Context, urls []string) map[string]int {
 	log := log.FromContext(ctx)
 
 	result := make(map[string]int)
+	http.DefaultClient.Timeout = 2 * time.Second
 
-	for _, url := range urls {
-		resp, err := http.Get(url)
+	for _, u := range urls {
+		resp, err := http.Get(u)
 		if err != nil {
-			log.Error(err, "Failed to poll URL", "url", url)
-			result[url] = -1
+			if err.(*url.Error).Timeout() {
+				log.Info("URL is unreachable", "url", u)
+			} else {
+				log.Error(err, "Failed to poll URL", "url", u)
+			}
+			result[u] = -1
 			continue
 		}
 		defer resp.Body.Close()
-
-		result[url] = resp.StatusCode
+		log.Info("URL is reachable", "url", u, "HTTP Status code", resp.Status)
+		result[u] = resp.StatusCode
 	}
 
 	return result
